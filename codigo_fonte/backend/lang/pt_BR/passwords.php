33d3f5c04781e079948cac482301f8476ebd9051
Rennan Alves <rennandev7@gmail.com>
2026-07-24 10:05:07 -0400

    fix: correção para aparecer corretamente as mensagens quando tem erro de login ou erro de cadastro de usuário

---
+++ codigo_fonte/backend/lang/pt_BR/passwords.php
@@ -0,0 +1,9 @@
+<?php
+
+return [
+    'reset'     => 'Sua senha foi redefinida com sucesso.',
+    'sent'      => 'Enviamos um link para redefinição de senha para seu e-mail.',
+    'throttled' => 'Por favor, aguarde antes de tentar novamente.',
+    'token'     => 'O token de redefinição de senha é inválido.',
+    'user'      => 'Não encontramos um usuário com esse endereço de e-mail.',
+];
